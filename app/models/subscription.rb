133500e20756856faaa34e890b84d25aa3700c7b
Wesley Garcia <wes@alltradestech.com>
2022-09-13 13:28:08 -0700

    Implement code for relationships and validations in database models

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,11 @@
 class Subscription < ApplicationRecord
   belongs_to :customer
   belongs_to :tea
+
+  validates_presence_of(:title)
+  validates_numericality_of(:price) # in US cents
+  validates_presence_of(:status)
+  validates_numericality_of(:frequency) # in weeks
+
+  enum status: %i[active cancelled]
 end
--- app/models/tea.rb
+++ app/models/tea.rb
@@ -1,2 +1,9 @@
 class Tea < ApplicationRecord
+  has_many(:subscriptions)
+  has_many(:customers, through: :subscriptions)
+
+  validates_presence_of(:title)
+  validates_presence_of(:description)
+  validates_numericality_of(:temperature) # in fahrenheit
+  validates_numericality_of(:brew_time) # in seconds
 end
