818caad9dbfda76e94eed16bc966ce99088264d9
Wesley Garcia <wes@alltradestech.com>
2022-09-13 13:27:05 -0700

    Add validation and relationship tests for database models

--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Subscription, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'relationships' do
+    it { should belong_to(:customer) }
+    it { should belong_to(:tea) }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_numericality_of(:price) }
+    it { should validate_presence_of(:status) }
+    it { should validate_numericality_of(:frequency) }
+    it { should define_enum_for(:status).with_values([:active, :cancelled])}
+  end
 end
