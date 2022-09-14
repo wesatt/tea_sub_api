2401a0934c318f3439f08086cfcfb8c6eadb2b84
Wesley Garcia <wes@alltradestech.com>
2022-09-13 17:18:09 -0700

    Add error handling method and error serializer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,6 @@
 class ApplicationController < ActionController::API
+  def error_handler(error_message, status = :bad_request)
+    response_hash = ErrorSerializer.format_error(error_message)
+    render json: response_hash, status: status
+  end
 end
