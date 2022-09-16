d9635dfcfd36d9e1df8f8d1bebf5fd6526fa83c0
Wesley Garcia <wes@alltradestech.com>
2022-09-15 22:15:53 -0700

    Add JSON API Serializer instead of hand rolling data

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::SubscriptionsController < ApplicationController
   def create
     subscription = Subscription.new(subscription_params)
     if subscription.save
-      render json: SubscriptionSerializer.format_subscription(subscription), status: :created
+      render json: SubscriptionSerializer.new(subscription), status: :created
     else
       error_handler(subscription.errors.messages, :bad_request)
     end
@@ -11,7 +11,7 @@ class Api::V1::SubscriptionsController < ApplicationController
   def destroy
     subscription = Subscription.find_by(customer_id: params[:customer_id], id: params[:id])
     if subscription&.cancelled!
-      render json: SubscriptionSerializer.format_subscription(subscription), status: :accepted
+      render json: SubscriptionSerializer.new(subscription), status: :accepted
     else
       error_handler({:'Customer id' => ['and subscription id do not match']}, :bad_request)
     end
@@ -19,14 +19,10 @@ class Api::V1::SubscriptionsController < ApplicationController
 
   def index
     subs = current_customer.subscriptions
-    render json: SubscriptionSerializer.format_subscriptions(subs), status: :accepted
+    render json: SubscriptionSerializer.new(subs), status: :accepted
   end
 
   private
-
-  # def current_tea
-  #   t ||= Tea.find(params[:tea_id])
-  # end
 
   def current_customer
     c ||= Customer.find(params[:customer_id])
